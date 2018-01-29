3ce9c414744a923fe33a3330a6097039b62dde31
anexus18 <galatoloalessio@gmail.com>
2018-01-29 16:03:00 +0100

    Added actions to the number button

--- app/src/main/java/com/example/galat/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/galat/myapplication/MainActivity.kt
@@ -3,14 +3,61 @@ package com.example.galat.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        initializeNumberButton()
+        initializeOpButton()
     }
 
+    fun initializeNumberButton(){
+        fun addNumber(number: Long) {
+            val shortOut = output.text.toString().toLong()
+            if (shortOut == 0L) {
+                output.text = number.toString()
+            } else {
+                output.text = (shortOut * 10L + number).toString()
+            }
+        }
+
+        b_0.setOnClickListener {
+            addNumber(0)
+        }
+        b_1.setOnClickListener {
+            addNumber(1)
+        }
+        b_2.setOnClickListener {
+            addNumber(2)
+        }
+        b_3.setOnClickListener {
+            addNumber(3)
+        }
+        b_4.setOnClickListener {
+            addNumber(4)
+        }
+        b_5.setOnClickListener {
+            addNumber(5)
+        }
+        b_6.setOnClickListener {
+            addNumber(6)
+        }
+        b_7.setOnClickListener {
+            addNumber(7)
+        }
+        b_8.setOnClickListener {
+            addNumber(8)
+        }
+        b_9.setOnClickListener {
+            addNumber(9)
+        }
+    }
+
+    fun initializeOpButton(){
+
+    }
 }
